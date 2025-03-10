e7003f31eb42f39de7257c10b15d6a29d7b3cf25
Jocean <kauen1999@gmail.com>
2025-03-10 15:13:15 -0300

    inicando a criação do context e provider

---
+++ src/context/GlobalContext.js
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+
+export const UserContext = createContext();
+export const TodoContext = createContext();
+export const PostContext = createContext();
+export const AlbumContext = createContext();
+export const PhotoContext = createContext();
